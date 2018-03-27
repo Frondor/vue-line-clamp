6fb9176877a21961f4f0f5dcefd0b7efa47571dc
Federico Vázquez <Frondor@users.noreply.github.com>
2018-03-27 18:05:52 -0300

    transpile to ES5 (#14)
    * transpile to ES5 (#13)
    
    * Bump version

--- dist/vue-line-clamp.umd.js
+++ dist/vue-line-clamp.umd.js
@@ -4,83 +4,83 @@
 	(global.VueLineClamp = factory());
 }(this, (function () { 'use strict';
 
-const css = 'display:block;display:-webkit-box;-webkit-box-orient:vertical;overflow:hidden;text-overflow:ellipsis';
-const currentValueProp = "vLineClampValue";
+var css = 'display:block;display:-webkit-box;-webkit-box-orient:vertical;overflow:hidden;text-overflow:ellipsis';
+var currentValueProp = "vLineClampValue";
 
 function defaultFallbackFunc(el, bindings, lines) {
-  if(lines){
-    let lineHeight = parseInt(bindings.arg);
+  if (lines) {
+    var lineHeight = parseInt(bindings.arg);
     if (isNaN(lineHeight)) {
       console.warn('line-height argument for vue-line-clamp must be a number (of pixels), falling back to 16px');
       lineHeight = 16;
     }
 
-    let maxHeight = lineHeight * lines;
+    var maxHeight = lineHeight * lines;
 
-    el.style.maxHeight = maxHeight ? maxHeight+'px' : '';
+    el.style.maxHeight = maxHeight ? maxHeight + 'px' : '';
     el.style.overflowX = 'hidden';
-    el.style.lineHeight = lineHeight+'px'; // to ensure consistency
+    el.style.lineHeight = lineHeight + 'px'; // to ensure consistency
   } else {
     el.style.maxHeight = el.style.overflowX = '';
   }
 }
 
-const truncateText = function (el, bindings, useFallbackFunc) {
-  let lines = parseInt(bindings.value);
+var truncateText = function truncateText(el, bindings, useFallbackFunc) {
+  var lines = parseInt(bindings.value);
   if (isNaN(lines)) {
     console.error('Parameter for vue-line-clamp must be a number');
-    return
-  }
-  else if (lines !== el[currentValueProp]) {
+    return;
+  } else if (lines !== el[currentValueProp]) {
     el[currentValueProp] = lines;
 
     if (useFallbackFunc) {
       useFallbackFunc(el, bindings, lines);
-    }
-    else {
+    } else {
       el.style.webkitLineClamp = lines ? lines : '';
     }
   }
 };
 
-const VueLineClamp = {
-  install (Vue, options) {
+var VueLineClamp = {
+  install: function install(Vue, options) {
     options = Object.assign({
       importCss: false
     }, options);
 
     if (options.importCss) {
-      const stylesheets = window.document.styleSheets,
-        rule = `.vue-line-clamp{${css}}`;
+      var stylesheets = window.document.styleSheets,
+          rule = '.vue-line-clamp{' + css + '}';
       if (stylesheets && stylesheets[0] && stylesheets.insertRule) {
         stylesheets.insertRule(rule);
       } else {
-        let link = window.document.createElement('style');
+        var link = window.document.createElement('style');
         link.id = 'vue-line-clamp';
         link.appendChild(window.document.createTextNode(rule));
         window.document.head.appendChild(link);
       }
     }
 
-    const useFallbackFunc =
-    "webkitLineClamp" in document.body.style
-        ? undefined
-        : options.fallbackFunc || defaultFallbackFunc;
+    var useFallbackFunc = "webkitLineClamp" in document.body.style ? undefined : options.fallbackFunc || defaultFallbackFunc;
 
     Vue.directive('line-clamp', {
       currentValue: 0,
-      bind (el) {
+      bind: function bind(el) {
         if (!options.importCss) {
           el.style.cssText += css;
-        }
-        else {
+        } else {
           el.classList.add('vue-line-clamp');
         }
-
       },
-      inserted: (el, bindings) => truncateText(el, bindings, useFallbackFunc),
-      updated: (el, bindings) => truncateText(el, bindings, useFallbackFunc),
-      componentUpdated: (el, bindings) => truncateText(el, bindings, useFallbackFunc)
+
+      inserted: function inserted(el, bindings) {
+        return truncateText(el, bindings, useFallbackFunc);
+      },
+      updated: function updated(el, bindings) {
+        return truncateText(el, bindings, useFallbackFunc);
+      },
+      componentUpdated: function componentUpdated(el, bindings) {
+        return truncateText(el, bindings, useFallbackFunc);
+      }
     });
   }
 };
